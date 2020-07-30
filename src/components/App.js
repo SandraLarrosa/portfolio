cf24476f682a912509bae3fde7e2f9a009f206fc
Alexandra <sandra.larrosa88@gmail.com>
2020-07-30 16:28:59 +0200

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../stylesheets/App.scss';
 
+const hi = '<h1>Hola Mundo</h1>';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
+    <div className='App'>
+      <h1>{hi}</h1>
     </div>
   );
 }
