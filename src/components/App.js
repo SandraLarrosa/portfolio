47f074b53d01725a39fe9c7289c8edca9d70c0f7
Alexandra <sandra.larrosa88@gmail.com>
2020-07-30 16:12:46 +0200

    install React

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
